574ce0a693b7044a5818c0f6d2f38e87e7bfcc72
john-kim-k <sokum5918@gmail.com>
2026-05-02 17:18:16 +0900

    add mexc seo page to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,25 +3,24 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://rebatemax.net";
 
-return [
-  {
-    url: baseUrl,
-    lastModified: new Date(),
-    changeFrequency: "weekly",
-    priority: 1,
-  },
-
-  {
-    url: `${baseUrl}/events`,
-    lastModified: new Date(),
-    changeFrequency: "weekly",
-    priority: 0.8,
-  },
-
-  {
-    url: `${baseUrl}/mexc-referral-code`,
-    lastModified: new Date(),
-    changeFrequency: "weekly",
-    priority: 0.9,
-  },
-];
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/events`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/mexc-referral-code`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+  ];
+}
