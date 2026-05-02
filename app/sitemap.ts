de84d27f198dd57a7b6af5b55dcdd8cc843d9ddc
john-kim-k <sokum5918@gmail.com>
2026-05-02 17:34:49 +0900

    add weex seo page

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,24 +3,29 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://rebatemax.net";
 
-  return [
-    {
-      url: baseUrl,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: `${baseUrl}/events`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/mexc-referral-code`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.9,
-    },
-  ];
-}
+return [
+  {
+    url: baseUrl,
+    lastModified: new Date(),
+    changeFrequency: "weekly",
+    priority: 1,
+  },
+  {
+    url: `${baseUrl}/events`,
+    lastModified: new Date(),
+    changeFrequency: "weekly",
+    priority: 0.8,
+  },
+  {
+    url: `${baseUrl}/mexc-referral-code`,
+    lastModified: new Date(),
+    changeFrequency: "weekly",
+    priority: 0.9,
+  },
+  {
+    url: `${baseUrl}/weex-referral-code`,
+    lastModified: new Date(),
+    changeFrequency: "weekly",
+    priority: 0.9,
+  },
+];
